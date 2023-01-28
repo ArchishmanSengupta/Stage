97e87a0e6ea6224dd3fb0247bc152e60c6eb5bc0
Archishman Sengupta <archishman1808@gmail.com>
2023-01-28 20:23:02 +0530

    modular code

--- lib/main.dart
+++ lib/main.dart
@@ -25,72 +25,80 @@ class StageVideoSelector extends StatefulWidget {
 }
 
 class _StageVideoSelectorState extends State<StageVideoSelector> {
+
   String _selectedQuality = '720p';
   String stream360p = 'https://res.cloudinary.com/dri88yrck/video/upload/v1674902569/360_ydin5t.mp4';
   String stream480p = 'https://res.cloudinary.com/dri88yrck/video/upload/v1674902570/480_inbgl6.mp4';
   String stream1080p = 'https://res.cloudinary.com/dri88yrck/video/upload/v1674902581/1080_qb5n5g.mp4';
   String stream4kp = 'https://res.cloudinary.com/dri88yrck/video/upload/v1674902572/4k_kqhzha.webm';
   String stream8kp='https://res.cloudinary.com/dri88yrck/video/upload/v1674902572/8k_zkstrt.webm';
-  
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
-  late int _playBackTime;
-  //The values that are passed when changing quality
-  late Duration newCurrentPosition;
+
+  late List<Map<String, dynamic>> _qualities;
+
+  late VideoPlayerController videoPlayerController;
+  late Future<void> initializeVideoPlayerFuture;
+  late int playBackTime;
+  late Duration currentPosition;
   
   @override
   void initState() {
-    _controller = VideoPlayerController.network(stream8kp);
-    _controller.addListener(() {
+    _qualities = [
+      {"name": "Super Ultra HD 8K", "value": stream8kp, "icon": Icons.diamond, "text": "VIP Service"},
+      {"name": "Ultra HD 4K", "value": stream4kp, "icon": Icons.diamond, "text": "VIP Service"},
+      {"name": "HD 1080P", "value": stream1080p},      {"name": "Better 480P", "value": stream480p},
+      {"name": "Good 360P", "value": stream360p},
+    ];
+    videoPlayerController = VideoPlayerController.network(stream8kp);
+    videoPlayerController.addListener(() {
       setState(() {
-        _playBackTime = _controller.value.position.inSeconds;
+        playBackTime = videoPlayerController.value.position.inSeconds;
       });
     });
-    _initializeVideoPlayerFuture = _controller.initialize();
+    initializeVideoPlayerFuture = videoPlayerController.initialize();
     super.initState();
   }
 
   @override
   void dispose() {
-    _initializeVideoPlayerFuture;
-    _controller.pause().then((_) {
-      _controller.dispose();
+    initializeVideoPlayerFuture;
+    videoPlayerController.pause().then((_) {
+      videoPlayerController.dispose();
     });
     super.dispose();
   }
 
-  Future<bool> _clearPrevious() async {
-    await _controller.pause();
+  Future<bool> clearPrevious() async {
+    await videoPlayerController.pause();
     return true;
   }
-
-  Future<void> _initializePlay(String videoPath) async {
-    _controller = VideoPlayerController.network(videoPath);
-    _controller.addListener(() {
+  
+  Future<void> initializePlay(String videoPath) async {
+    videoPlayerController = VideoPlayerController.network(videoPath);
+    videoPlayerController.addListener(() {
       setState(() {
-        _playBackTime = _controller.value.position.inSeconds;
+        playBackTime = videoPlayerController.value.position.inSeconds;
       });
     });
-    _initializeVideoPlayerFuture = _controller.initialize().then((_) {
-      _controller.seekTo(newCurrentPosition);
-      _controller.play();
+    initializeVideoPlayerFuture = videoPlayerController.initialize().then((_) {
+      videoPlayerController.seekTo(currentPosition);
+      videoPlayerController.play();
     });
   }
 
-  void _getValuesAndPlay(String videoPath) {
-    newCurrentPosition = _controller.value.position;
-    _startPlay(videoPath);
-    print(newCurrentPosition.toString());
+  void getValuesAndPlay(String videoPath) {
+    currentPosition = videoPlayerController.value.position;
+    startPlay(videoPath);
+    print(currentPosition.toString());
   }
   
 
-  Future<void> _startPlay(String videoPath) async {
+ Future<void> startPlay(String videoPath) async {
     setState(() {
-      _initializeVideoPlayerFuture ;
+      initializeVideoPlayerFuture ;
     });
     Future.delayed(const Duration(milliseconds: 20), () {
-      _clearPrevious().then((_) {
-        _initializePlay(videoPath);
+      clearPrevious().then((_) {
+        initializePlay(videoPath);
       });
     });
   }
@@ -98,183 +106,128 @@ class _StageVideoSelectorState extends State<StageVideoSelector> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initializeVideoPlayerFuture,
+      future: initializeVideoPlayerFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return Stack(
             children: <Widget>[
               Center(
                 child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  // Use the VideoPlayer widget to display the video.
-                  child: VideoPlayer(_controller),
+                  aspectRatio: videoPlayerController.value.aspectRatio,
+                  child: VideoPlayer(videoPlayerController),
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  color: Colors.transparent,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Container(
-                        child: FloatingActionButton(
-                          backgroundColor: Colors.transparent,
-                          onPressed: () {
-                            // Wrap the play or pause in a call to `setState`. This ensures the
-                            // correct icon is shown.
-                            setState(() {
-                              // If the video is playing, pause it.
-                              if (_controller.value.isPlaying) {
-                                _controller.pause();
-                              } else {
-                                // If the video is paused, play it.
-                                _controller.play();
-                              }
-                            });
-                          },
-                          // Display the correct icon depending on the state of the player.
-                          child: Icon(
-                            _controller.value.isPlaying
-                                ? Icons.pause
-                                : Icons.play_arrow,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 150,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.5),
-                            borderRadius: BorderRadius.circular(15)
-                        ),
-                        child: Padding(
-                            padding: EdgeInsets.all(5),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                    DefaultTextStyle(
-                                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                                      child: Text(
-                                          _controller.value.position
-                                              .toString()
-                                              .split('.')
-                                              .first
-                                              .padLeft(8, "0"),
-                                          
-                                      ),
-                                    ),
-                                ],
-                            ),
-                        ),
-                    ),
-                      FloatingActionButton(
-                        backgroundColor: Colors.transparent,
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return SimpleDialog(
-                                  title: Text("Video Quality"),
-                                  children: <Widget>[
-                                    SimpleDialogOption(
-                                      onPressed: () {
-                                        setState(() {
-                                          _selectedQuality = "Super Ultra HD 8K";
-                                        });
-                                        _getValuesAndPlay(stream8kp);
-                                        Navigator.pop(context);
-                                      },
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text("Super Ultra HD 8K"),
-                                          Row(
-                                            // ignore: prefer_const_literals_to_create_immutables
-                                            children: [
-                                              Icon(Icons.diamond, size: 12),
-                                              SizedBox(width: 10,),
-                                              Text("VIP Service"),
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                    SimpleDialogOption(
-                                      onPressed: () {
-                                        setState(() {
-                                          _selectedQuality = "4K";
-                                        });
-                                        _getValuesAndPlay(stream4kp);
-                                        Navigator.pop(context);
-                                      },
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text("Ultra HD 4K"),
-                                          Row(
-                                            // ignore: prefer_const_literals_to_create_immutables
-                                            children: [
-                                              Icon(Icons.diamond, size: 12),
-                                              SizedBox(width: 10,),
-                                              Text("VIP Service"),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    SimpleDialogOption(
-                                      onPressed: () {
-                                        setState(() {
-                                          _selectedQuality = "1080p";
-                                        });
-                                        _getValuesAndPlay(stream1080p);
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text("HD 1080P"),
-                                    ),
-                                    SimpleDialogOption(
-                                      onPressed: () {
-                                        setState(() {
-                                          _selectedQuality = "Better 480P";
-                                        });
-                                        _getValuesAndPlay(stream480p);
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text("Better 480P"),
-                                    ),
-                                    SimpleDialogOption(
-                                      onPressed: () {
-                                        setState(() {
-                                          _selectedQuality = "Good 360P";
-                                        });
-                                        _getValuesAndPlay(stream360p);
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text("Good 360P"),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          },
-                          child: const Icon(
-                            Icons.settings,
-                            color: Colors.white,
-                          ),
-                        )
-                    ],
-                  ),
-                ),
-              ),
+              _buildControls()
             ],
           );
         } else {
-          // If the VideoPlayerController is still initializing, show a
-          // loading spinner.
-          return const Center(child: CircularProgressIndicator());
+          return Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+
+  Widget _buildControls() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        color: Colors.transparent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _buildPlayPauseButton(),
+            _buildCurrentTime(),
+            _buildQualityButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlayPauseButton() {
+    return FloatingActionButton(
+      backgroundColor: Colors.transparent,
+      onPressed: () {
+        setState(() {
+          if (videoPlayerController.value.isPlaying) {
+            videoPlayerController.pause();
+          } else {
+            videoPlayerController.play();
+          }
+        });
+      },
+      child: Icon(
+        videoPlayerController.value.isPlaying
+            ? Icons.pause
+            : Icons.play_arrow,
+      ),
+    );
+  }
+  Widget _buildCurrentTime() {
+    return Container(
+      width: 150,
+      height: 50,
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.5), borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DefaultTextStyle(
+              style: const TextStyle(
+                  color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+              child: Text(
+                videoPlayerController.value.position.toString().split('.').first.padLeft(8, "0"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buildQualityButton() {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: Text("Choose Streaming Quality"),
+              children: _qualities.map((quality) {
+                return SimpleDialogOption(
+                  onPressed: () {
+                    setState(() {
+                      _selectedQuality = quality['name'];
+                    });
+                    getValuesAndPlay(quality['value']);
+                    Navigator.pop(context);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(quality['name']),
+                      quality.containsKey("icon")
+                      ? Row(
+                            children: [
+                              Icon(quality['icon'], size: 12),
+                              const SizedBox(width: 10,),
+                              Text(quality['text']),
+                            ],
+                        )
+                      : Container(),
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          },
+        );
+      },
+      child: const Icon(
+        Icons.settings,
+        color: Colors.white,
+      ),
     );
   }
 }
